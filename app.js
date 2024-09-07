3022ee9a8aea9b4b713e82ce2eda6f8972360f61
Nico <nico0210.bayrhofer@gmail.com>
2024-09-07 19:50:54 +0200

    Deine Commit-Nachricht

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use((req, res, next) => {
   console.log('In the mid3dle2ware!');
-   res.send('<h1>Hello fr2om Express!</h1>');
+   res.send('<h1>Hello fr2om E2xpress!</h1>');
 });
 
 app.listen(3000);
